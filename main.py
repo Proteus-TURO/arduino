55aef79aa075d79c9bd1e7edbf0f6abc350748e1
Kilian Kier <kierkilian@gmail.com>
2023-01-27 12:05:01 +0000

    added ros2 logic

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 import serial
 import time
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
 
-ser = serial.Serial(port='/dev/ttyUSB0', baudrate=9600)
 
-while(True):
-    ser.write('on'.encode('utf-8'))
-    time.sleep(1)
-    ser.write('off'.encode('utf-8'))
-    time.sleep(1)
+class ArduinoConnection(Node):
+    def __init__(self):
+        super().__init__('arduino_connection')
+        self.serial_connection = serial.Serial(port='/dev/ttyUSB0', baudrate=9600)
+        self.subscription = self.create_subscription(String, 'controller/led', self.callback, 10)
+
+    def callback(self, msg):
+        self.serial_connection.write(msg.data.encode('utf-8'))
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    arduino_connection = ArduinoConnection()
+    rclpy.spin(arduino_connection)
+    arduino_connection.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
 
