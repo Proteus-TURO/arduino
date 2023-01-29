cc6c27cf948e1bc6307c670b5a63f922f79b09fe
Kilian Kier <kierkilian@gmail.com>
2023-01-29 21:06:32 +0100

    Added arduino code

---
+++ arduino.ino
@@ -0,0 +1,26 @@
+const int BUFFER_SIZE = 100;
+char buf[BUFFER_SIZE];
+
+void setup() {
+  Serial.begin(9600);
+  ledcSetup(0, 5000 ,8);
+  ledcAttachPin(2, 0);
+}
+
+void led(int brightness) {
+  ledcWrite(0, brightness);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    int cmd = Serial.read();
+    if (cmd == 0) {
+      while (Serial.available() == 0) {
+        usleep(100);
+      }
+      int brightness = Serial.read();
+      Serial.println(brightness);
+      led(brightness);
+    }
+  }
+}
