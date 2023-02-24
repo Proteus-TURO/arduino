5c2ab868b8ddbba2e45954a81b2eba5919d492c2
Kilian Kier <kierkilian@gmail.com>
2023-02-24 12:44:31 +0100

    Update arduino.ino

--- arduino.ino
+++ arduino.ino
@@ -1,22 +1,40 @@
+const int LED_PIN = 13;
 void setup() {
   Serial.begin(9600);
-  ledcSetup(0, 5000 ,8);
-  ledcAttachPin(2, 0);
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void led(int brightness) {
-  ledcWrite(0, brightness);
+  Serial.println(brightness);
+  analogWrite(LED_PIN, brightness);
+}
+
+void drive(float linear_x, float linear_y, float angular_z) {
+  Serial.println(linear_x);
+  Serial.println(linear_y);
+  Serial.println(angular_z);
 }
 
 void loop() {
   if (Serial.available() > 0) {
     int cmd = Serial.read();
+    Serial.println(cmd);
     if (cmd == 0) {
       while (Serial.available() == 0) {
-        usleep(100);
+        delay(1);
       }
       int brightness = Serial.read();
       led(brightness);
+    } else if (cmd == 1) {
+      while (Serial.available() < 12) {
+        delay(1);
+      }
+      char buffer[12];
+      Serial.readBytes(buffer, 12);
+      float x = ((float *) buffer)[0];
+      float y = ((float *) buffer)[1];
+      float z = ((float *) buffer)[2];
+      drive(x, y, z);
     }
   }
 }
