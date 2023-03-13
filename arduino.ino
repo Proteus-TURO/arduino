6a4334f2788d739c863d74194badfc853931d217
Marcit2 <marcitrenki@gmail.com>
2023-03-13 08:15:46 +0100

    changed arduino.ino
    Signed-off-by: Marcit2 <marcitrenki@gmail.com>

--- arduino.ino
+++ arduino.ino
@@ -1,20 +1,27 @@
 const int LED_PIN = 13;
-const int MOTOR_V_L = 6; const int IN1_V = 4; const int IN2_V = 2;/*Motor vorne links D5(PWM)->EN2,D1->IN3,D0->IN4*/
-const int MOTOR_V_R = 5; const int IN3_V = 1; const int IN4_V = 0;/*Motor vorne rechts   D6(PWM)->EN1,D4->IN1,D2->IN2*/
-const int MOTOR_H_L = 11; const int IN1_H = 13; const int IN2_H = 12;/*Motor hinten links D10(PWM)->EN2,D8->IN3,D7->IN4 */
-const int MOTOR_H_R = 10; const int IN3_H = 8; const int IN4_H = 7; /*Motor hinten rechts D11(PWM)->EN1,D13->IN1,D12->IN2*/
+const int MOTOR_V_L[] = {6/*EN1*/,4/*IN1*/,2/*IN2*/};     /*Motor vorne links D6(PWM)->EN1,D1->IN1,D0->IN2*/
+const int MOTOR_V_R[] = {5/*EN2*/,0/*IN3*/,1/*IN4*/};     /*Motor vorne rechts D5(PWM)->EN2,D4->IN3,D2->IN4*/
+const int MOTOR_H_L[] = {10/*EN2*/,8/*IN3*/,7/*IN4*/};    /*Motor hinten links D10(PWM)->EN2,D8->IN3,D7->IN4 */
+const int MOTOR_H_R[] = {11/*EN1*/,13/*IN1*/,12/*IN2*/};  /*Motor hinten rechts D11(PWM)->EN1,D13->IN1,D12->IN2*/
+const int WHEEL_SEPARATION_WIDTH = 1;
+const int WHEEL_SEPARATION_LENGTH = 1;
+const int ticksPerMeter = 1;
 
 void setup() {
   Serial.begin(9600);
   pinMode(LED_PIN, OUTPUT);
-  pinMode(IN1_V,OUTPUT);
-  pinMode(IN2_V,OUTPUT);
-  pinMode(IN3_V,OUTPUT);
-  pinMode(IN4_V,OUTPUT);
-  pinMode(IN1_H,OUTPUT);
-  pinMode(IN2_H,OUTPUT);
-  pinMode(IN3_H,OUTPUT);
-  pinMode(IN4_H,OUTPUT);
+  pinMode(MOTOR_V_L[0],OUTPUT);
+  pinMode(MOTOR_V_L[1],OUTPUT);
+  pinMode(MOTOR_V_L[2],OUTPUT);
+  pinMode(MOTOR_V_L[0],OUTPUT);
+  pinMode(MOTOR_V_R[1],OUTPUT);
+  pinMode(MOTOR_V_R[2],OUTPUT);
+  pinMode(MOTOR_V_L[0],OUTPUT);
+  pinMode(MOTOR_H_L[1],OUTPUT);
+  pinMode(MOTOR_H_L[2],OUTPUT);
+  pinMode(MOTOR_V_L[0],OUTPUT);
+  pinMode(MOTOR_H_R[1],OUTPUT);
+  pinMode(MOTOR_H_R[2],OUTPUT);
 }
 
 void led(int brightness) {
@@ -22,66 +29,78 @@ void led(int brightness) {
   analogWrite(LED_PIN, brightness);
 }
 
-void set_duty_cycle(int motor,int duty_cycle /*in Prozent*/) {
+void brake(const int motor[]) {
 
-  int IN1;
-  int IN2;
-
-  switch(motor) {
-
-    case MOTOR_V_L:
-
-        IN1 = IN1_V;
-        IN2 = IN2_V;
-        break;
-
-    case MOTOR_V_R:
-
-        IN1 = IN3_V;
-        IN2 = IN4_V;
-        break;
-
-    case MOTOR_H_L:
-
-        IN1 = IN1_H;
-        IN2 = IN2_H;
-        break;
-
-    case MOTOR_H_R:
-
-        IN1 = IN3_H;
-        IN2 = IN4_H;
-        break;
-    
-  }
-
-  if (duty_cycle < 0) {
-    duty_cycle = abs(buty_cycle);
-    digitalWrite(IN1,HIGH);
-    digitalWrite(IN2,LOW);
-    analogWrite(motor,255);
-  }else if (duty_cycle > 100) {
-    digitalWrite(IN1,LOW);
-    digitalWrite(IN2,HIGH);
-    analogWrite(motor,255);
-  }else if (duty_cycle == 0) {
-    digitalWrite(IN1,LOW);
-    digitalWrite(IN2,LOW);
-    analogWrite(motor,duty_cycle);
-  }else {
-    digitalWrite(IN1,LOW);
-    digitalWrite(IN2,HIGH);
-    analogWrite(motor,(duty_cycle*2,55));
-  }
+    digitalWrite(motor[0],HIGH);
+    digitalWrite(motor[1],LOW);
+    digitalWrite(motor[2],LOW); 
   
 }
 
+void set_duty_cycle(const int motor[],int duty_cycle /*in Prozent*/) {
 
-void drive(float linear_x, float linear_y, float angular_z) {
+  /*range: -100% bis 100%*/
+  
+  if (duty_cycle < 0) {
+    duty_cycle = abs(duty_cycle);
+    if (duty_cycle > 100) {
+    analogWrite(motor[0],255);
+    digitalWrite(motor[1],LOW);
+    digitalWrite(motor[2],HIGH); 
+    }else {
+    analogWrite(motor[0],(duty_cycle*2.55));
+    digitalWrite(motor[1],LOW);
+    digitalWrite(motor[2],HIGH);
+    }
+  }else if (duty_cycle > 100) {
+    analogWrite(motor[0],255);
+    digitalWrite(motor[1],HIGH);
+    digitalWrite(motor[2],LOW);
+  }else if (duty_cycle == 0) {
+    analogWrite(motor[0],0);
+    digitalWrite(motor[1],LOW);
+    digitalWrite(motor[2],LOW);
+  }else {
+    analogWrite(motor[0],(duty_cycle*2.55));
+    digitalWrite(motor[1],HIGH);
+    digitalWrite(motor[2],LOW);
+  }
+    
+}
 
-  Serial.println(linear_x);
-  Serial.println(linear_y);
-  Serial.println(angular_z);
+void set_all_duty_cycle(int duty_cycle/*in Prozent*/) {
+  set_duty_cycle(MOTOR_V_L,duty_cycle);
+  set_duty_cycle(MOTOR_V_R,duty_cycle);
+  set_duty_cycle(MOTOR_H_L,duty_cycle);
+  set_duty_cycle(MOTOR_H_R,duty_cycle);
+}
+
+void set_v_duty_cycle(int duty_cycle/*in Prozent*/){
+  set_duty_cycle(MOTOR_V_L,duty_cycle);
+  set_duty_cycle(MOTOR_V_R,duty_cycle);
+}
+
+void set_h_duty_cycle(int duty_cycle/*in Prozent*/) {
+  set_duty_cycle(MOTOR_H_L,duty_cycle);
+  set_duty_cycle(MOTOR_H_R,duty_cycle);
+}
+
+void drive (float linear_x, float linear_y, float angular_z) {
+
+  int frontLeft = ticksPerMeter * (linear_x - linear_y - (WHEEL_SEPARATION_WIDTH + WHEEL_SEPARATION_LENGTH)*angular_z);
+  int frontRight = ticksPerMeter * (linear_x + linear_y + (WHEEL_SEPARATION_WIDTH + WHEEL_SEPARATION_LENGTH)*angular_z);
+  int rearLeft = ticksPerMeter * (linear_x + linear_y - (WHEEL_SEPARATION_WIDTH + WHEEL_SEPARATION_LENGTH)*angular_z);
+  int rearRight = ticksPerMeter * (linear_x - linear_y + (WHEEL_SEPARATION_WIDTH + WHEEL_SEPARATION_LENGTH)*angular_z);
+
+  set_duty_cycle(MOTOR_V_L,frontLeft);
+  set_duty_cycle(MOTOR_V_R,frontRight);
+  set_duty_cycle(MOTOR_H_L,rearLeft);
+  set_duty_cycle(MOTOR_H_R,rearRight);
+  
+  Serial.println(frontLeft);
+  Serial.println(frontRight);
+  Serial.println(rearLeft);
+  Serial.println(rearRight);
   
 }
 
