50264c1d26241e5c25c26713d80fc3256b21c9c6
Marcit2 <marcitrenki@gmail.com>
2023-03-13 09:52:02 +0100

    added maxMotorSpeed
    Signed-off-by: Marcit2 <marcitrenki@gmail.com>

--- arduino.ino
+++ arduino.ino
@@ -3,9 +3,10 @@ const int MOTOR_V_L[] = {6/*EN1*/,4/*IN1*/,2/*IN2*/};     /*Motor vorne links D6
 const int MOTOR_V_R[] = {5/*EN2*/,0/*IN3*/,1/*IN4*/};     /*Motor vorne rechts D5(PWM)->EN2,D4->IN3,D2->IN4*/
 const int MOTOR_H_L[] = {10/*EN2*/,8/*IN3*/,7/*IN4*/};    /*Motor hinten links D10(PWM)->EN2,D8->IN3,D7->IN4 */
 const int MOTOR_H_R[] = {11/*EN1*/,13/*IN1*/,12/*IN2*/};  /*Motor hinten rechts D11(PWM)->EN1,D13->IN1,D12->IN2*/
-const int WHEEL_SEPARATION_WIDTH = 1  /*Distance between the two wheels on the same axis (meters)*/;
-const int WHEEL_SEPARATION_LENGTH = 1 /*Distance between the front and rear axis (meters)*/;
-const int ticksPerMeter = 1;          /*Number of encoder ticks per meter of travel*/
+const int WHEEL_SEPARATION_WIDTH = 1;  /*Distance between the two wheels on the same axis (meters)*/
+const int WHEEL_SEPARATION_LENGTH = 1; /*Distance between the front and rear axis (meters)*/
+const int ticksPerMeter = 1;           /*Number of encoder ticks per meter of travel*/
+const int maxSpeed = 10000;
 
 void setup() {
   Serial.begin(9600);
@@ -36,7 +37,6 @@ void brake(const int motor[]) {
 }
 
 void set_duty_cycle(const int motor[],int duty_cycle /*in Prozent*/) {
-
   /*range: -100% bis 100%*/
   if (duty_cycle < 0) {
     duty_cycle = abs(duty_cycle);
@@ -62,7 +62,6 @@ void set_duty_cycle(const int motor[],int duty_cycle /*in Prozent*/) {
       digitalWrite(motor[1],HIGH);
       digitalWrite(motor[2],LOW);
   }
-    
 }
 
 void set_all_duty_cycle(int duty_cycle/*in Prozent*/) {
@@ -83,11 +82,19 @@ void set_h_duty_cycle(int duty_cycle/*in Prozent*/) {
 }
 
 void drive (float linear_x, float linear_y, float angular_z) {
-
+  
   int frontLeft = ticksPerMeter * (linear_x - linear_y - (WHEEL_SEPARATION_WIDTH + WHEEL_SEPARATION_LENGTH)*angular_z);
   int frontRight = ticksPerMeter * (linear_x + linear_y + (WHEEL_SEPARATION_WIDTH + WHEEL_SEPARATION_LENGTH)*angular_z);
   int rearLeft = ticksPerMeter * (linear_x + linear_y - (WHEEL_SEPARATION_WIDTH + WHEEL_SEPARATION_LENGTH)*angular_z);
   int rearRight = ticksPerMeter * (linear_x - linear_y + (WHEEL_SEPARATION_WIDTH + WHEEL_SEPARATION_LENGTH)*angular_z);
+
+  if (max(max(frontLeft,frontRight),max(rearLeft,rearRight)) > maxSpeed) {
+    float factor = maxSpeed / max(max(frontLeft,frontRight),max(rearLeft,rearRight));
+    frontLeft *= factor;
+    frontRight *= factor;
+    rearLeft *= factor;
+    rearRight *= factor;
+  }
 
   set_duty_cycle(MOTOR_V_L,frontLeft);
   set_duty_cycle(MOTOR_V_R,frontRight);
